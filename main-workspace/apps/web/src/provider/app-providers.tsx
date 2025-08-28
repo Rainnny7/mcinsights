94ed71a0f3688b739d96ece9e72001f61be44090
Braydon <braydonrainnny@gmail.com>
2025-08-28 02:40:57 -0400

    disable the default context menus

--- main-workspace/apps/web/src/provider/app-providers.tsx
+++ main-workspace/apps/web/src/provider/app-providers.tsx
@@ -2,19 +2,34 @@
 
 import { queryClient, trpc, trpcClient } from "@/lib/trpc";
 import { QueryClientProvider } from "@tanstack/react-query";
-import type { ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
 import { Toaster } from "../components/ui/sonner";
 import { ThemeProvider } from "./theme-provider";
 
-const AppProviders = ({ children }: { children: ReactNode }) => (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <QueryClientProvider client={queryClient}>
-            <trpc.Provider client={trpcClient} queryClient={queryClient}>
-                {children}
-            </trpc.Provider>
-            {/* <ReactQueryDevtools /> */}
-        </QueryClientProvider>
-        <Toaster richColors />
-    </ThemeProvider>
-);
+const AppProviders = ({ children }: { children: ReactNode }) => {
+    // Disable the default context menu
+    useEffect(() => {
+        const handleContextMenu = (event: MouseEvent) => {
+            const target: HTMLElement = event.target as HTMLElement;
+            if (!target.closest("[data-custom-context-menu]")) {
+                event.preventDefault();
+            }
+        };
+        document.addEventListener("contextmenu", handleContextMenu);
+        return () =>
+            document.removeEventListener("contextmenu", handleContextMenu);
+    }, []);
+
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <QueryClientProvider client={queryClient}>
+                <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                    {children}
+                </trpc.Provider>
+                {/* <ReactQueryDevtools /> */}
+            </QueryClientProvider>
+            <Toaster richColors />
+        </ThemeProvider>
+    );
+};
 export default AppProviders;

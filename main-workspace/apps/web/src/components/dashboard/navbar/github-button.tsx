282231ca99609d92fd754d526f2db5ec810354b9
Braydon <braydonrainnny@gmail.com>
2025-08-28 01:53:38 -0400

    update the gh button

--- main-workspace/apps/web/src/components/dashboard/navbar/github-button.tsx
+++ main-workspace/apps/web/src/components/dashboard/navbar/github-button.tsx
@@ -1,39 +1,27 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import Image from "next/image";
+import { GithubIcon } from "lucide-react";
 import Link from "next/link";
 import type { ReactElement } from "react";
 import { appConfig } from "../../../app/config";
 import SimpleTooltip from "../../simple-tooltip";
 import { Button } from "../../ui/button";
 
-const GitHubButton = (): ReactElement => {
-    const { theme } = useTheme();
-    return (
-        <SimpleTooltip content="View the source on GitHub" side="bottom">
-            <Link
-                href={`https://github.com/${appConfig.githubUrl}`}
-                target="_blank"
-                draggable={false}
+const GitHubButton = (): ReactElement => (
+    <SimpleTooltip content="View the source on GitHub" side="bottom">
+        <Link
+            href={`https://github.com/${appConfig.githubUrl}`}
+            target="_blank"
+            draggable={false}
+        >
+            <Button
+                className="size-8.5 border border-border rounded-full"
+                variant="ghost"
+                size="icon"
             >
-                <Button
-                    className="size-8.5 border border-border rounded-full"
-                    variant="ghost"
-                    size="icon"
-                >
-                    <Image
-                        src={`/media/logo/${
-                            theme === "light" ? "github-dark" : "github"
-                        }.png`}
-                        alt="GitHub"
-                        width={22}
-                        height={22}
-                        draggable={false}
-                    />
-                </Button>
-            </Link>
-        </SimpleTooltip>
-    );
-};
+                <GithubIcon className="size-4.5" />
+            </Button>
+        </Link>
+    </SimpleTooltip>
+);
 export default GitHubButton;

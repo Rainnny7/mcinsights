a5ef507f1b1b3b9b651f850ee50ae536c39f638c
Braydon <braydonrainnny@gmail.com>
2025-08-30 17:27:53 -0400

    auto redirect logged in users to /dashboard

--- main-workspace/apps/web/src/app/(pages)/page.tsx
+++ main-workspace/apps/web/src/app/(pages)/page.tsx
@@ -3,21 +3,30 @@ import LoginCard from "@/components/auth/login-card";
 import Background from "@/components/background";
 import type { Metadata } from "next";
 import BasicFooter from "../../components/basic-footer";
+import { getSession } from "../../lib/auth";
+import type { ReactElement } from "react";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Login",
     description: "Login to your account",
 };
 
-const LoginPage = async () => (
-    <div className="min-h-screen flex justify-center items-center">
-        <Background />
-        <div className="relative z-10">
-            <FadeInAnimation>
-                <LoginCard />
-            </FadeInAnimation>
+const LoginPage = async (): Promise<ReactElement> => {
+    const session = await getSession();
+    if (session) {
+        redirect("/dashboard");
+    }
+    return (
+        <div className="min-h-screen flex justify-center items-center">
+            <Background />
+            <div className="relative z-10">
+                <FadeInAnimation>
+                    <LoginCard />
+                </FadeInAnimation>
+            </div>
+            <BasicFooter />
         </div>
-        <BasicFooter />
-    </div>
-);
+    );
+};
 export default LoginPage;

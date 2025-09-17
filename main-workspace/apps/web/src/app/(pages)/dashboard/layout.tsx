874cdc7b4bbf112de301d2b75a07556332637177
Braydon <braydonrainnny@gmail.com>
2025-09-17 03:14:20 -0400

    and this

--- main-workspace/apps/web/src/app/(pages)/dashboard/layout.tsx
+++ main-workspace/apps/web/src/app/(pages)/dashboard/layout.tsx
@@ -28,7 +28,7 @@ const DashboardLayout = async ({
         >
             <DashboardProvider user={user}>
                 <NextTopLoader color="var(--color-dashboard-toploader-color)" />
-                <div className="dark:bg-[url('/media/background/diagonal-lines.svg')] -z-10">
+                <div className="dark:bg-[url('/media/diagonal-lines.svg')] -z-10">
                     <DashboardNavbar user={user} />
                     <div className="min-h-screen mx-auto max-w-screen-2xl px-5 pt-34 pb-10 space-y-5 z-10">
                         <div className="w-fit">

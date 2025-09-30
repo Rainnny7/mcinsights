d3fe00ca80d4330e7403995e1df4b958305d25ed
Braydon <braydonrainnny@gmail.com>
2025-09-29 23:24:27 -0400

    update the background gradient a bit

--- main-workspace/apps/web/src/components/background.tsx
+++ main-workspace/apps/web/src/components/background.tsx
@@ -5,13 +5,17 @@ import type { ReactElement } from "react";
 
 const Background = (): ReactElement => (
     <motion.div
-        className="absolute inset-0 z-0"
+        className="fixed inset-0 z-0"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.25, ease: "easeInOut" }}
+        // style={{
+        //     background:
+        //         "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(0, 170, 0, 0.25), transparent 70%), var(--dashboard-alternative-background)",
+        // }}
         style={{
             background:
-                "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(0, 170, 0, 0.25), transparent 70%), var(--background)",
+                "radial-gradient(circle at 20% 20%, var(--dashboard-alternative-background) 0%, transparent 40%), radial-gradient(circle at 80% 30%, var(--dashboard-alternative-background) 0%, transparent 40%), linear-gradient(120deg, #0f170e 0%, var(--dashboard-alternative-background) 100%)",
         }}
     />
 );

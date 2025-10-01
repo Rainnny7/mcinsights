06f4c8857a36ecd1b1c70839fa21a3f3d559560a
Braydon <braydonrainnny@gmail.com>
2025-10-01 06:20:36 -0400

    reactCompiler is no longer experimental

--- main-workspace/apps/web/next.config.ts
+++ main-workspace/apps/web/next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: "standalone",
     reactStrictMode: true,
+    reactCompiler: true,
     poweredByHeader: false,
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
     devIndicators: false,
     experimental: {
-        reactCompiler: true,
         authInterrupts: true,
     },
     transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
